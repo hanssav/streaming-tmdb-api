53f4f11f034b3353097f7a555209152fc9651755
hanssav <handihandi40@gmail.com>
2025-10-19 20:19:07 +0700

    add invalidateQuerys  update data
    Signed-off-by: hanssav <handihandi40@gmail.com>

--- src/app/movies/[id]/hooks/use-update-data.ts
+++ src/app/movies/[id]/hooks/use-update-data.ts
@@ -1,11 +1,18 @@
-import { useAddToFavorites, useAllFavorites } from '@/hooks/useMovies';
+import {
+  movieKeys,
+  useAddToFavorites,
+  useAllFavorites,
+} from '@/hooks/useMovies';
 import { APIConfiguration } from '@/lib/constants';
+import { useQueryClient } from '@tanstack/react-query';
 import React from 'react';
 
 export const useUpdateData = (id: number) => {
+  const account_id = APIConfiguration.mock_account_id;
   const { data: favorites } = useAllFavorites(id);
   const [isFavorited, setIsFavorited] = React.useState(false);
   const addToFavorite = useAddToFavorites();
+  const queryClient = useQueryClient();
 
   React.useEffect(() => {
     setIsFavorited(favorites?.id === Number(id));
@@ -15,14 +22,24 @@ export const useUpdateData = (id: number) => {
     setIsFavorited((prev) => {
       const newStatus = !prev;
 
-      addToFavorite.mutate({
-        account_id: APIConfiguration.mock_account_id,
-        body: {
-          media_type: 'movie',
-          media_id: id,
-          favorite: newStatus,
+      addToFavorite.mutate(
+        {
+          account_id: APIConfiguration.mock_account_id,
+          body: {
+            media_type: 'movie',
+            media_id: id,
+            favorite: newStatus,
+          },
         },
-      });
+        {
+          onSuccess: () => {
+            queryClient.invalidateQueries({
+              queryKey: movieKeys.allFavorites(account_id),
+              refetchType: 'all',
+            });
+          },
+        }
+      );
 
       return newStatus;
     });
